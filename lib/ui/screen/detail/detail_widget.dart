fef785dd7a6077f1975ede67e3011cd1d15fe41b
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-04-11 13:40:56 +0700

    Create detail screen

---
+++ lib/ui/screen/detail/detail_widget.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:moviedb_flutter/ui/screen/detail/detail_state.dart';
+
+class Detail extends StatefulWidget {
+  @override
+  DetailState createState() => DetailState();
+}
