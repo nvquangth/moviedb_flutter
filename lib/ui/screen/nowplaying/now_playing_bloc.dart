4df76cdba8e85b0b6bb34ebf073222aa19a5c7f0
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-04-19 10:15:47 +0700

    Update Nowplaying

--- lib/ui/screen/nowplaying/now_playing_bloc.dart
+++ lib/ui/screen/nowplaying/now_playing_bloc.dart
@@ -1,2 +1,21 @@
+import 'package:moviedb_flutter/data/model/movie_respone.dart';
+import 'package:moviedb_flutter/di/app_injection.dart';
+import 'package:moviedb_flutter/ui/base/base_bloc_provider.dart';
 import 'package:rxdart/rxdart.dart';
 
+class NowPlayingBloc extends BaseBloc {
+  final _repository = injection.provideRepository();
+  final _moviesFetcher = PublishSubject<MovieResponse>();
+
+  Observable<MovieResponse> get movies => _moviesFetcher.stream;
+
+  @override
+  void dispose() {
+    _moviesFetcher.close();
+  }
+
+  fetchMovies(int page) async {
+    MovieResponse response = await _repository.getNowPlaying(page);
+    _moviesFetcher.sink.add(response);
+  }
+}
--- lib/ui/screen/nowplaying/now_playing_state.dart
+++ lib/ui/screen/nowplaying/now_playing_state.dart
@@ -1,63 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:moviedb_flutter/data/model/movie.dart';
 import 'package:moviedb_flutter/data/model/movie_respone.dart';
-import 'package:moviedb_flutter/di/app_injection.dart';
 import 'package:moviedb_flutter/ui/screen/detail/detail_widget.dart';
+import 'package:moviedb_flutter/ui/screen/nowplaying/now_playing_bloc.dart';
 import 'package:moviedb_flutter/ui/screen/nowplaying/now_playing_widget.dart';
-import 'package:toast/toast.dart';
 
 class NowPlayingState extends State<NowPlaying> {
-  final _injection = AppInjection();
-  var isLoading = true;
-  List<Movie> movies;
+  final bloc = NowPlayingBloc();
 
   @override
   void initState() {
     super.initState();
-
-    setState(() {
-      isLoading = true;
-    });
-
-    _injection.provideRepository().getNowPlaying(1, onSuccess, onFail);
+    bloc.fetchMovies(1);
   }
 
-  void onSuccess(MovieResponse movieResponse) {
-    setState(() {
-      isLoading = false;
-      this.movies = movieResponse.movies;
-    });
-  }
-
-  void onFail(Exception e) {
-    setState(() {
-      isLoading = false;
-    });
-    Toast.show(e.toString(), context);
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: isLoading
-            ? CircularProgressIndicator()
-            : ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: movies.length,
-                itemBuilder: (context, i) {
-                  return _buildRow(movies[i], context);
-                }),
+        child: StreamBuilder(
+            stream: bloc.movies,
+            builder: (context, AsyncSnapshot<MovieResponse> snapshot) {
+              if (snapshot.hasData) {
+                return _buildList(snapshot);
+              } else if (snapshot.hasError) {
+                return Text(snapshot.error.toString());
+              }
+              return CircularProgressIndicator();
+            }),
       ),
     );
+  }
+
+  Widget _buildList(AsyncSnapshot<MovieResponse> snapshot) {
+    return ListView.builder(
+        itemCount: snapshot.data.movies.length,
+        itemBuilder: (context, i) {
+          return _buildRow(snapshot.data.movies[i], context);
+        });
   }
 
   Widget _buildRow(Movie movie, BuildContext context) {
     var url = "http://image.tmdb.org/t/p/w185" + movie.posterPath;
 
     _gotoDetail() {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => Detail(movie: movie,)));
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => Detail(
+                movie: movie,
+              )));
 
 //      Navigator.of(context).pushNamed('/detail', arguments: movie);
     }
