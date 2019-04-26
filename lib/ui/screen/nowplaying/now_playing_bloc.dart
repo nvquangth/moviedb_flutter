581da220ac99c1f751a15f6ab022bfff273b8bf8
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-04-26 13:12:01 +0700

    Update favorite now playing

--- lib/ui/screen/nowplaying/now_playing_bloc.dart
+++ lib/ui/screen/nowplaying/now_playing_bloc.dart
@@ -1,14 +1,23 @@
+import 'package:moviedb_flutter/data/model/movie.dart';
 import 'package:moviedb_flutter/data/model/movie_respone.dart';
 import 'package:moviedb_flutter/di/app_injection.dart';
 import 'package:moviedb_flutter/ui/base/base_bloc_provider.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:moviedb_flutter/data/model/movie.dart';
 
 class NowPlayingBloc extends BaseBloc {
+  MovieResponse _movieResponse;
   final _repository = injection.provideRepository();
   final _moviesFetcher = PublishSubject<MovieResponse>();
 
   Observable<MovieResponse> get movies => _moviesFetcher.stream;
+
+  final PublishSubject<Movie> inFavoriteSubject = PublishSubject<Movie>();
+
+  Sink<Movie> get inFavoriteMovie => inFavoriteSubject.sink;
+
+  NowPlayingBloc() {
+    inFavoriteSubject.listen(_updateFavorite);
+  }
 
   @override
   void dispose() async {
@@ -18,15 +27,29 @@ class NowPlayingBloc extends BaseBloc {
 
   fetchMovies(int page) async {
     MovieResponse response = await _repository.getNowPlaying(page);
+    _movieResponse = response;
     if (response.movies == null || response.movies.isEmpty) return;
 
     List<Movie> movies = response.movies;
 
     for (int i = 0; i < movies.length; i++) {
       Movie movie = await _repository.getMovie(false, movies[i].id);
-      movie == null ? movies[i].isFavorite = false : movies[i].isFavorite = true;
+      movie == null
+          ? movies[i].isFavorite = false
+          : movies[i].isFavorite = true;
     }
     response.movies = movies;
     _moviesFetcher.sink.add(response);
+  }
+
+  _updateFavorite(Movie movie) {
+    for (int i = 0; i < _movieResponse.movies.length; i++) {
+      if (movie.id == _movieResponse.movies[i].id) {
+        _movieResponse.movies[i].isFavorite =
+            !_movieResponse.movies[i].isFavorite;
+        _moviesFetcher.sink.add(_movieResponse);
+        return;
+      }
+    }
   }
 }
